e7d95b3c43b15f7bf0beeb47d42f78a76ed94479
vitor <vitorekrlol@gmail.com>
2023-01-02 19:53:21 -0300

    exercicios

---
+++ divide_conquer_exer/majority_element/majority_element.java
@@ -0,0 +1,25 @@
+package majority_element;
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        return dfs(nums, 0, n -1);
+    }
+    public int dfs(int[] nums, int i, int j){
+        if(i == j) return nums[i];
+        int m = (i + j)/2;
+        int left = dfs(nums, i,m);
+        int right = dfs(nums, m+1, j);
+        
+        if(left == right) return left;
+        
+        return count(nums, left, i , m) > count(nums, right, m + 1, j) ? left : right;
+    }
+    
+    public int count(int[] ar, int c, int i, int j){
+        int res = 0;
+        for(int k = i; k <= j;k++){
+            if(ar[k] == c) res++;
+        }
+        return res;
+    }
+}
---
+++ divide_conquer_exer/max_subarray/max_subarray.java
@@ -0,0 +1,33 @@
+package max_subarray;
+class Solution {
+    public int maxSubArray(int[] nums) {
+        return recur(nums, 0, nums.length - 1);
+    }
+    
+    public int recur(int[] nums, int i , int j){
+        if(i == j) return nums[i];
+        int m = (i+j)/2;
+        int l = recur(nums, i, m);
+        int r = recur(nums, m+1,j);
+        
+        int mer = mer(nums, m);
+        return Math.max(l, Math.max(r, mer));
+    }
+    
+    public int mer(int[] nums,int m){
+        int lmax = Integer.MIN_VALUE;
+        int l = 0;
+        for(int i = m; i >= 0; i--){
+            l += nums[i];
+            lmax = Math.max(lmax, l);
+        }
+        int rmax = Integer.MIN_VALUE;
+        int r = 0;
+        for(int i = m + 1; i < nums.length ; i++){
+            r += nums[i];
+            rmax = Math.max(rmax, r);
+        }
+        return Math.max(lmax + rmax,Math.max(lmax, rmax));
+    }
+    
+}
